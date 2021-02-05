434b531c61ce0d13b8444054e6150bfbba76cdc5
Brannon Glover <brannon.glover@dcblox.com>
2021-02-05 15:09:00 -0500

    More updates to styling

--- src/components/Planet/Planet.styled.js
+++ src/components/Planet/Planet.styled.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
 export const PlanetStyleP = styled.p`
-  margin: 6px 0;
+  margin: .3em 0;
   color: #999;
 `;
 
 export const PlanetStyleH3 = styled.h3`
-  margin: 10px 0;
+  margin: .5em 0;
   color: #ffff57;
 `;
